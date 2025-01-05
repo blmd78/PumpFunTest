fe40b715ce6447a59fa5dda63ffa236f5cdcd68d
DummySkin <62141701+DummySkin@users.noreply.github.com>
2025-01-05 21:00:04 +0100

    modification to accept the subgraph httpspecifically

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
+          content="default-src 'self'; connect-src 'self' http://35.234.119.105:8000 ws: wss: https: http:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: http:;"
         />
       </Head>
       <body>
