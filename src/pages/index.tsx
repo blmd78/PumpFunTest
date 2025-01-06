5b96ad674223349924470302657ac3507cde258b
DummySkin <62141701+DummySkin@users.noreply.github.com>
2025-01-06 21:50:11 +0100

    roll back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -252,7 +252,7 @@ const Home: React.FC = () => {
       <SEO
         title="Create and Trade Memecoins Easily on Likeaser."
         description="The ultimate platform for launching and trading memecoins on Rootstock. Create your own tokens effortlessly and engage in fair, dynamic trading."
-        image="seo/home.png"
+        image="seo/home.jpg"
       />
       <HowItWorksPopup />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
